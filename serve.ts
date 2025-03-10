0494bbd0b72eb2ae45d5cfc4c7660bf9e069aa95
Geoffroy Warin <code@geowarin.com>
2025-03-10 14:54:15 +0100

    refactor: template

--- serve.ts
+++ serve.ts
@@ -1,5 +1,8 @@
 import {serveDir} from "jsr:@std/http/file-server";
-import {dirname} from "https://deno.land/std@0.114.0/path/mod.ts";
+import {dirname} from "jsr:@std/path";
+import {Eta} from "jsr:@eta-dev/eta";
+
+const eta = new Eta({ views: Deno.cwd() + "/views/", cache: false });
 
 async function* getFilesRecursively(dir: string): AsyncIterable<string> {
   for await (const entry of Deno.readDir(dir)) {
@@ -12,36 +15,42 @@ async function* getFilesRecursively(dir: string): AsyncIterable<string> {
   }
 }
 
+type Icon = { path: string; content: string };
+
+type IconData = {
+  dir: string;
+  icons: Icon[];
+};
+
+async function* getIconsData(): AsyncIterable<IconData> {
+  let currentDir = "";
+  let currentIcons: Icon[] = [];
+
+  for await (const icon of getFilesRecursively("icons")) {
+    const parent = dirname(icon);
+
+    if (parent !== currentDir) {
+      if (currentDir !== "") {
+        yield { dir: currentDir, icons: currentIcons };
+      }
+      currentDir = parent;
+      currentIcons = [];
+    }
+    const svgContent = await Deno.readTextFile(icon);
+    currentIcons.push({ path: icon, content: svgContent });
+  }
+  if (currentDir !== "") {
+    yield { dir: currentDir, icons: currentIcons };
+  }
+}
+
 Deno.serve(async (req: Request) => {
   const pathname = new URL(req.url).pathname;
 
   if (pathname === "/gallery") {
-    let html = `<html><head>
-<style>
-svg {
-  width: 100px;
-  height: 100px;
-}
-</style></head><body>`;
+    const iconsData = getIconsData();
 
-    let currentDir = "";
-    for await (const icon of getFilesRecursively("icons")) {
-      const parent = dirname(icon);
-      if (parent !== currentDir) {
-        if (currentDir !== "") {
-          html += `</div>`;
-        }
-        currentDir = parent;
-        html += `<h1>${currentDir}</h1>`;
-        html += `<div style="display: flex; flex-direction: row; flex-wrap: wrap;">`;
-      }
-
-      const svgContent = await Deno.readTextFile(icon);
-      html +=
-        `<div title="${icon}">${svgContent}</div>`;
-    }
-
-    html += "</body></html>";
+    const html = await eta.renderAsync("gallery.eta", { iconsData });
     return new Response(html, {
       headers: { "Content-Type": "text/html" },
     });
